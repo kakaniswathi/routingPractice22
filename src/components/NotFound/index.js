ebf798cd5d71513b6177a58dc398106b2a52bd50
kakaniswathi <kswathi2177@gmail.com>
2023-02-18 13:50:58 +0000

    routingPractice

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,13 @@
+// Write your JS code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="about-section">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      alt="NotFound"
+    />
+    <h1>Not Found</h1>
+  </div>
+)
+export default NotFound
